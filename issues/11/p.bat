8e4fb05cf4eca50fca282ba8e103017dae1fe2c8
littleflute <littleflute@163.com>
2020-05-22 21:01:20 -0700

    r60.bat*

--- issues/11/p.bat
+++
@@ -1,7 +0,0 @@
-echo p.bat V0.0. 11 ;
-
-
-ffplay %1
-
-
-echo debug:  ...
--- issues/11/r60.bat
+++ issues/11/r60.bat
@@ -1,6 +1,6 @@
-echo V0.0. 11 ;
+echo V0.0. 12 ;
 
 for /l %%x in (1, 1, 60) do (
    echo %%x 
-   curl http://localhost:8080/image/clock?time=10:00:%%x -o %%x.jpg
+   curl http://localhost:8080/image/clock?time=10:00:%%x -o %%x.jpg -b cookie
 )
